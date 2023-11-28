9c4ea71c8083ca7f9b04141b46809e31c2b3eb1e
AmithAKulk <95955499+AmithAKulk@users.noreply.github.com>
2023-11-28 15:33:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+function updateTimer() {
+	const timerElement = document.getElementById('timer');
+	const now = new Date();
+	
+	const options = { 
+		weekday: 'long', 
+	    year: 'numeric', 
+	    month: 'long', 
+	    day: 'numeric', 
+	    hour: '2-digit', 
+	    minute: '2-digit', 
+	    second: '2-digit', 
+	    timeZoneName: 'short' 
+	};
+
+	const formattedTime = now.toLocaleDateString('en-US', options);
+
+	timerElement.textContent = formattedTime;
+}
+
+updateTimer();
+
+setInterval(updateTimer, 1000);
